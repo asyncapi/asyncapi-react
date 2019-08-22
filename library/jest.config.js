8a31391fbbc0dd108bd4ac883e23e01011785917
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-08-22 14:48:10 +0200

    remove uneeded tsconfig.test.json

--- library/jest.config.js
+++ library/jest.config.js
@@ -5,11 +5,6 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.test.json',
-    },
-  },
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
 };
