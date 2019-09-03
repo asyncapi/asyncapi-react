2e3c1dc7b86659acbee3f0361d25d94ec72dd431
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-09-03 10:15:59 +0200

    .

--- playground/src/common/defaultConfig.ts
+++ playground/src/common/defaultConfig.ts
@@ -2,7 +2,7 @@ export const defaultConfig: string = `{
   "show": {
     "info": true,
     "servers": true,
-    "security": true,
+    "security": false,
     "messages": true,
     "schemas": true,
     "channels": true,
