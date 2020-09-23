549ffd959f12f55331a7c8fbdfede8e21940f82f
E516981 <claude.gex@sbb.ch>
2020-09-23 17:27:39 +0200

    updated to match new project structure

--- library/webpack.config.js
+++ library/webpack.config.js
@@ -1,36 +1,39 @@
 const path = require('path');
 module.exports = {
+  entry: {
+    'async-api-web-component': './src/index.ts',
+    'async-api-web-component.min': './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'AsyncApiWebComponent',
+    libraryExport: 'default',
+    umdNamedDefine: true,
+  },
 
-    entry: {
-        'async-api-web-component': './src/index.ts',
-        'async-api-web-component.min': './src/index.ts'
-    },
-    output: {
-        path: path.resolve(__dirname, 'lib'),
-        filename: '[name].js',
-        libraryTarget: 'umd',
-        library: 'AsyncApiWebComponent',
-        libraryExport: 'default',
-        umdNamedDefine: true
-    },
+  mode: 'production',
+  target: 'web',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  devtool: 'source-map',
 
-    target: 'web',
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/,
+        query: {
+          declaration: true,
+        },
+      },
+    ],
+  },
 
-
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader',
-                exclude: /node_modules/,
-                query: {
-                    declaration: true,
-                }
-            }
-        ]
-    }
+  node: {
+    fs: 'empty',
+  },
 };
