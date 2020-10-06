4ca5fdd2db4113c39058cfcba5eed22054662cb9
depimomo <depimomo@users.noreply.github.com>
2020-10-07 04:30:51 +0700

    test: remove unnecessary undefined on test (#161)
    * Remove unecessary undefined in toKebabCase.test.ts
    
    * Change test name
    
    Co-authored-by: Monica Devi Kristiadi <monica.kristiadi@btpn.com>

--- library/src/helpers/toKebabCase.ts
+++ library/src/helpers/toKebabCase.ts
@@ -6,6 +6,7 @@ export function toKebabCase(str?: string): string {
   const matched = str.match(
     /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g,
   );
+
   if (matched) {
     return matched.map(x => x.toLowerCase()).join('-');
   }
