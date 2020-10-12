69610f98ac7ca3987860de302bc93d9b859304a6
sanskar-p <sanskarpatro2012@gmail.com>
2020-10-12 15:55:22 +0530

    renamed CodeEditor to CodeEditorComponent

--- playground/src/components/index.ts
+++ playground/src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as Navigation } from './Navigation';
-export { default as CodeEditor } from './CodeEditor';
+export { default as CodeEditorComponent } from './CodeEditorComponent';
 export { default as FetchSchema } from './FetchSchema';
 export { default as Tabs } from './Tabs';
 export { default as Tab } from './Tab';
