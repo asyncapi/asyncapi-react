12d19a24bda0409f9261a69261107bd772f6ef46
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2020-11-12 14:40:18 +0100

    feat: add web-component (#175)

---
+++ web-component/semantic-release.js
@@ -0,0 +1,24 @@
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        pkgRoot: 'web-component',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
