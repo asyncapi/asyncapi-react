6f447592211140af523101a9941e9e5355ca5a5d
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-03-23 22:11:02 +0530

    Change SplitWrapper to use functional component

--- playground/src/components/SplitWrapper.tsx
+++ playground/src/components/SplitWrapper.tsx
@@ -1,36 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Split from 'react-split';
 
-export interface WrapperProps {
-  children: React.ReactNode;
-}
-
-class SplitWrapper extends Component<WrapperProps> {
-  render() {
-    return (
-      <Split
-        style={{
-          width: '100%',
-          height: 'calc(100vh - 50px)',
-          minHeight: 'calc(100vh - 50px)',
-          display: 'flex',
-          background: '#f3f4f5',
-        }}
-        gutter={() => {
-          const gutter = document.createElement('div');
-          gutter.onmouseover = () => (gutter.style.cursor = 'ew-resize');
-          return gutter;
-        }}
-        gutterStyle={() => ({
-          backgroundColor: 'gray',
-          width: '7px',
-        })}
-        minSize={250}
-      >
-        {this.props.children}
-      </Split>
-    );
-  }
-}
+const SplitWrapper: React.FunctionComponent = ({ children }) => (
+  <Split
+    style={{
+      width: '100%',
+      height: 'calc(100vh - 50px)',
+      minHeight: 'calc(100vh - 50px)',
+      display: 'flex',
+      background: '#f3f4f5',
+    }}
+    gutter={() => {
+      const gutter = document.createElement('div');
+      gutter.onmouseover = () => (gutter.style.cursor = 'ew-resize');
+      return gutter;
+    }}
+    gutterStyle={() => ({
+      backgroundColor: 'gray',
+      width: '7px',
+    })}
+    minSize={250}
+  >
+    {children}
+  </Split>
+);
 
 export default SplitWrapper;
