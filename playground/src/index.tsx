99473a8dfedf880d0f37f7ad642377f4cb708fea
Matatjahu <urbanczyk.maciej.95@gmail.com>
2021-04-15 11:29:12 +0200

    rebase

--- playground/src/index.tsx
+++ playground/src/index.tsx
@@ -5,7 +5,7 @@ import Playground from './Playground';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import '@asyncapi/react-component/lib/styles/fiori.css';
-import '@asyncapi/react-component/lib/styles/styles.css';
+import '@asyncapi/react-component/styles/default.min.css';
 import './common/icons';
 
 ReactDOM.render(<Playground />, document.getElementById('root'));
