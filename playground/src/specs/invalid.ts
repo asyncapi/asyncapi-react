b7da160fb2a89832bf1345c87818405b4b334a25
Matatjahu <urbanczyk.maciej.95@gmail.com>
2021-05-25 16:35:57 +0200

    update deps

--- playground/src/specs/invalid.ts
+++ playground/src/specs/invalid.ts
@@ -1,37 +1,30 @@
 export const invalid = `
+asyncapi: '2.0.0'
+info:
+  title: Recursive API
+  version: '1.0.0'
+
+channels:
+  recursive-items:
+    subscribe:
+      message:
+        $ref: '#/components/messages/RecursiveItem'
+
 components:
   messages:
-    RideUpdated:
+    RecursiveItem:
       payload:
-        type: object
-      schemaFormat: application/vnd.aai.asyncapi+json;version=2.0.0
-      contentType: application/json
-    PaymentCharged:
-      payload:
-        type: object
-      schemaFormat: application/vnd.aai.asyncapi+json;version=2.0.0
-      contentType: application/json
-channels:
-  'taxinyc/backoffice/payment/charged/v1/{payment_status}/{driver_id}/{passenger_id}':
-    publish:
-      x-scs-function-name: processPayment
-      message:
-        $ref: '#/components/messages/PaymentCharged'
-    parameters:
-      driver_id:
-        schema:
+        $ref: "#/components/schemas/RecursiveItem"
+
+  schemas:
+    RecursiveItem:
+      type: object
+      properties:
+        name:
           type: string
-      payment_status:
-        schema:
-          type: string
-  'taxinyc/ops/ride/updated/v1/{ride_status}/{driver_id}/{passenger_id}/{current_latitude}/{current_longitude}/{long_property}':
-    subscribe:
-      x-scs-function-name: processPayment
-      x-scs-destination: test/taxinyc/PaymentProcessorQueue
-      message:
-        $ref: '#/components/messages/RideUpdated'
-asyncapi: 2.0.0
-info:
-  title: ProcessPayment
-  version: 0.0.1
+          format: time
+        subItems:
+          type: array
+          items:
+            $ref: '#/components/schemas/RecursiveItem'
 `;
