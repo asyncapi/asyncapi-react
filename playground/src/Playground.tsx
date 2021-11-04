f77ebde4a52974798513d7a03ef1d7cd918108d7
Matatjahu <urbanczyk.maciej.95@gmail.com>
2021-11-04 16:18:14 +0100

    feat: handle circular refs

--- playground/src/Playground.tsx
+++ playground/src/Playground.tsx
@@ -17,7 +17,7 @@ import {
 import { defaultConfig, parse, debounce } from './common';
 import * as specs from './specs';
 
-const defaultSchema = specs.circular;
+const defaultSchema = specs.streetlights;
 
 interface State {
   schema: string;
