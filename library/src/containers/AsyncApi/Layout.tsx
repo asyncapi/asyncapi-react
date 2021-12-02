a3a6136fc00cb666f4edd56a8921db336a3846c3
Kai Szybiak <kaiszybiak@gmail.com>
2021-12-02 16:10:34 +0100

    feat: introduce scoped css for default styles

--- library/src/containers/AsyncApi/Layout.tsx
+++ library/src/containers/AsyncApi/Layout.tsx
@@ -38,24 +38,26 @@ const AsyncApiLayout: React.FunctionComponent<Props> = ({
   return (
     <ConfigContext.Provider value={config}>
       <SpecificationContext.Provider value={asyncapi}>
-        <section
-          className={`${
-            divWidth <= 1280 ? 'container:xl' : 'container:base'
-          } relative md:flex bg-white`}
-          id={config.schemaID || undefined}
-          ref={ref}
-        >
-          {config.show?.sidebar && <Sidebar config={config.sidebar} />}
-          <div className="panel--center relative py-8 flex-1">
-            <div className="relative z-10">
-              {config.show?.errors && error && <Error error={error} />}
-              {config.show?.info && <Info />}
-              {config.show?.servers && <Servers />}
-              {config.show?.operations && <Operations />}
-              {config.show?.messages && <Messages />}
-              {config.show?.schemas && <Schemas />}
+        <section className="aui-root">
+          <div
+            className={`${
+              divWidth <= 1280 ? 'container:xl' : 'container:base'
+            } relative md:flex bg-white leading-normal`}
+            id={config.schemaID || undefined}
+            ref={ref}
+          >
+            {config.show?.sidebar && <Sidebar config={config.sidebar} />}
+            <div className="panel--center relative py-8 flex-1">
+              <div className="relative z-10">
+                {config.show?.errors && error && <Error error={error} />}
+                {config.show?.info && <Info />}
+                {config.show?.servers && <Servers />}
+                {config.show?.operations && <Operations />}
+                {config.show?.messages && <Messages />}
+                {config.show?.schemas && <Schemas />}
+              </div>
+              <div className="panel--right absolute top-0 right-0 h-full bg-gray-800" />
             </div>
-            <div className="panel--right absolute top-0 right-0 h-full bg-gray-800" />
           </div>
         </section>
       </SpecificationContext.Provider>
