49438cf7635d77ebab2c85e69f22adc1cf91bd1c
Kai Szybiak <kaiszybiak@gmail.com>
2021-12-06 11:29:26 +0100

    feat: introduce scoped css for default styles (#493)

--- library/postcss.config.js
+++ library/postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
     require('tailwindcss'),
     require('autoprefixer'),
     require('postcss-import'),
+    require('postcss-scopify')('.aui-root'),
     process.env.NODE_ENV === 'production' &&
       process.env.MINIFY_STYLES === 'true' &&
       require('cssnano')({
