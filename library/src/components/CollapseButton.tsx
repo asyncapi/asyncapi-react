f2c507da513cd9ad9a0f397488fb702f916f672e
Sergey Shishkin <sergey@shishkin.org>
2023-01-23 15:33:10 +0100

    Update library/src/components/CollapseButton.tsx
    Co-authored-by: Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>

--- library/src/components/CollapseButton.tsx
+++ library/src/components/CollapseButton.tsx
@@ -7,20 +7,20 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 const HiChevronRight = (props: SVGAttributes<SVGElement> = {}) => (
   // Copied from https://icon-sets.iconify.design/heroicons/chevron-right/
-  <svg
+  <svg 
+    stroke="currentColor" 
+    fill="currentColor" 
+    strokeWidth="0" 
+    viewBox="0 0 20 20" 
+    height="1em" 
+    width="1em" 
     xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
-    viewBox="0 0 24 24"
     {...props}
   >
-    <path
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="1.5"
-      d="m8.25 4.5l7.5 7.5l-7.5 7.5"
+    <path 
+      fillRule="evenodd" 
+      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" 
+      clipRule="evenodd"
     />
   </svg>
 );
