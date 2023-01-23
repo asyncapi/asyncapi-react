d9f280c6334b2c1a6cdbb3641c44f5d56b62bc49
Sergey Shishkin <sergey@shishkin.org>
2023-01-23 15:53:45 +0100

    chore: fix lint errors

--- library/src/components/CollapseButton.tsx
+++ library/src/components/CollapseButton.tsx
@@ -7,19 +7,19 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 const HiChevronRight = (props: SVGAttributes<SVGElement> = {}) => (
   // Copied from https://icon-sets.iconify.design/heroicons-solid/chevron-right/
-  <svg 
-    stroke="currentColor" 
-    fill="currentColor" 
-    strokeWidth="0" 
-    viewBox="0 0 20 20" 
-    height="1em" 
-    width="1em" 
+  <svg
+    stroke="currentColor"
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 20 20"
+    height="1em"
+    width="1em"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path 
-      fillRule="evenodd" 
-      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" 
+    <path
+      fillRule="evenodd"
+      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
       clipRule="evenodd"
     />
   </svg>
