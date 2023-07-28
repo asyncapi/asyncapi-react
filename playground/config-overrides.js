032869883ebf47ccd7f80dd616ed9f5aa5ef0bc8
Laupetin <jan@laupetin.net>
2023-07-28 15:13:15 +0200

    chore: Set Access-Control-Allow-Origin header for devserver

--- playground/config-overrides.js
+++ playground/config-overrides.js
@@ -1,9 +1,19 @@
 const path = require('path');
 
-module.exports = function override(config, env) {
-  config.resolve = Object.assign({}, config.resolve, {
-    modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
-  });
+module.exports = {
+  webpack: function override(config, env) {
+    config.resolve = Object.assign({}, config.resolve, {
+      modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
+    });
 
-  return config;
+    return config;
+  },
+  devServer: function(configFunction) {
+    return function(proxy, allowedHost) {
+      const config = configFunction(proxy, allowedHost);
+      config.headers = { 'Access-Control-Allow-Origin': '*' };
+
+      return config;
+    };
+  },
 };
