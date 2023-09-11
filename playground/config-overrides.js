1f779767544b8ef7908ba5445e4c39ae5a1590bd
Jonas Lagoni <jonas-lt@live.dk>
2023-09-11 16:25:00 +0200

    chore: fix playground build (#763)

--- playground/config-overrides.js
+++ playground/config-overrides.js
@@ -1,12 +1,50 @@
 const path = require('path');
 
-module.exports = function override(config, env) {
-  config.resolve = {
-    ...config.resolve,
-    ...{
-      modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
-    },
-  };
-
-  return config;
+module.exports = {
+  webpack: function(config, env) {
+    config.module.rules = config.module.rules.map(rootRule => {
+      if (rootRule.oneOf) {
+        rootRule.oneOf = rootRule.oneOf.map(rule => {
+          if ((rule.loader ?? '').includes('babel-loader/lib/index.js')) {
+            rule.use = [
+              path.join(
+                __dirname,
+                '../library/loaders/remove-hashbag-loader.js',
+              ),
+              {
+                loader: rule.loader,
+                options: {
+                  ...rule.options,
+                  plugins: [
+                    ...(rule.options.plugins ?? []),
+                    '@babel/plugin-proposal-class-properties',
+                  ],
+                },
+              },
+            ];
+            delete rule.loader;
+            delete rule.options;
+          }
+          return rule;
+        });
+      }
+      return rootRule;
+    });
+    config.resolve = {
+      ...config.resolve,
+      ...{
+        alias: {
+          'nimma/legacy$': path.resolve(
+            __dirname,
+            'node_modules/nimma/dist/legacy/cjs/index.js',
+          ),
+          'nimma/fallbacks$': path.resolve(
+            __dirname,
+            'node_modules/nimma/dist/cjs/fallbacks/index.js',
+          ),
+        },
+      },
+    };
+    return config;
+  },
 };
