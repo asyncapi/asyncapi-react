7a78b41a434e5bd0dd127fbf794c96d907f3dec5
Paras <mehtaparas1901@gmail.com>
2023-10-04 16:17:54 +0530

    refactor: remove the redundant-jump code

--- library/src/helpers/message.ts
+++ library/src/helpers/message.ts
@@ -56,8 +56,6 @@ export class MessageHelpers {
     if (payload && payload.examples()) {
       return payload.examples()?.map(example => ({ example }));
     }
-
-    return;
   }
 
   static getHeadersExamples(
@@ -87,7 +85,5 @@ export class MessageHelpers {
     if (headers && headers.examples()) {
       return headers.examples()?.map(example => ({ example }));
     }
-
-    return;
   }
 }
