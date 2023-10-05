06bb30ced591bf2aac1fa1df90b9305407566f18
Paras <mehtaparas1901@gmail.com>
2023-10-05 23:54:29 +0530

    refactor: added optional chain expressions

--- library/src/helpers/message.ts
+++ library/src/helpers/message.ts
@@ -53,7 +53,7 @@ export class MessageHelpers {
     }
 
     const payload = msg.payload();
-    if (payload && payload.examples()) {
+    if (payload?.examples()) {
       return payload.examples()?.map(example => ({ example }));
     }
 
@@ -84,7 +84,7 @@ export class MessageHelpers {
     }
 
     const headers = msg.headers();
-    if (headers && headers.examples()) {
+    if (headers?.examples()) {
       return headers.examples()?.map(example => ({ example }));
     }
 
