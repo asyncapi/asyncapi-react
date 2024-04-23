59b5eb0b3610ca0703f95a5b0fc8f123315a7e7c
Kenneth Aasan <k.aasan@sportradar.com>
2024-04-23 11:47:14 +0200

    chore: migrate from tslint to eslint

--- library/loaders/remove-hashbag-loader.js
+++ library/loaders/remove-hashbag-loader.js
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /**
  * Make sure code does not contain properties such as `#property`
  */
