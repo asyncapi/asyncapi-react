1855313506ba6248978aae06798ba1ac2d9abeff
ItshMoh <mohan.kumar.min22@itbhu.ac.in>
2024-10-08 14:40:33 +0530

    fixed the linting issue

--- playground/components/SplitWrapper.tsx
+++ playground/components/SplitWrapper.tsx
@@ -7,27 +7,27 @@ interface SplitWrapperProps {
 
 // react-split should be replaced with a React 18 friendly library or CSS
 const SplitWrapper = (props: SplitWrapperProps) => (
-    <Split
-      style={{
-        width: '100%',
-        height: 'calc(100vh - 50px)',
-        minHeight: 'calc(100vh - 50px)',
-        display: 'flex',
-        background: '#f3f4f5',
-      }}
-      gutter={() => {
-        const gutter = document.createElement('div');
-        gutter.onmouseover = () => (gutter.style.cursor = 'ew-resize');
-        return gutter;
-      }}
-      gutterStyle={() => ({
-        backgroundColor: 'gray',
-        width: '7px',
-      })}
-      minSize={250}
-    >
-      {props.children}
-    </Split>
+  <Split
+    style={{
+      width: '100%',
+      height: 'calc(100vh - 50px)',
+      minHeight: 'calc(100vh - 50px)',
+      display: 'flex',
+      background: '#f3f4f5',
+    }}
+    gutter={() => {
+      const gutter = document.createElement('div');
+      gutter.onmouseover = () => (gutter.style.cursor = 'ew-resize');
+      return gutter;
+    }}
+    gutterStyle={() => ({
+      backgroundColor: 'gray',
+      width: '7px',
+    })}
+    minSize={250}
+  >
+    {props.children}
+  </Split>
 );
 
 export default SplitWrapper;
