f5fc06413fe2a22092a3805d52c668cce9ef6fd4
ItshMoh <mohan.kumar.min22@itbhu.ac.in>
2024-10-08 14:38:36 +0530

    fixed linting issue

--- library/src/helpers/__tests__/schema.test.ts
+++ library/src/helpers/__tests__/schema.test.ts
@@ -896,11 +896,7 @@ describe('SchemaHelpers', () => {
 
     test('should not render title because title is undefined', () => {
       expect(
-        SchemaHelpers.applicatorSchemaName(
-          0,
-          FIRST_CASE,
-          OTHER_CASES,
-        ),
+        SchemaHelpers.applicatorSchemaName(0, FIRST_CASE, OTHER_CASES),
       ).toMatchSnapshot();
 
       expect(
