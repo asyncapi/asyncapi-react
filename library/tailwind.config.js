8dffe7c266bfe2596015f7a51c720f108e40174c
Sagar Seth <lightningsagar0@gmail.com>
2025-12-10 22:01:03 +0530

    fix(tailwind): restore current color to prevent breakage

--- library/tailwind.config.js
+++ library/tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   content: ['./src/**/*.tsx'], // or ./src/**/*.{js,jsx,ts,tsx}
   theme: {
     colors: {
+      current: 'currentColor',
+
       black: '#000',
       white: '#fff',
       gray: {
